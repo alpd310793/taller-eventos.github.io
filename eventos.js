721f2a6aa3101f31ea5523767999cdca2fc79c29
Ana Pérez <anaperezdecuadro@gmail.com>
2023-08-06 21:00:00 -0300

    Se Agrega Alerta al clickear en el Div

---
+++ eventos.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+    
+let div = document.getElementById("primerDiv");
+div.addEventListener("click", function() {
+    alert("Hola! Soy el div");
+});
+});
