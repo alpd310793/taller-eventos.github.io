9825c3295a7805471900c3df859c28ff85644a58
Ana Pérez <anaperezdecuadro@gmail.com>
2023-08-13 19:47:19 -0300

    solucion-click
    Se soluciona el problema de que al dar click en el botón,  se disparaba el evento click del div también

--- eventos.js
+++ eventos.js
@@ -1,8 +1,17 @@
 document.addEventListener('DOMContentLoaded', function () {
 
+
+   
     
 let div = document.getElementById("primerDiv");
 div.addEventListener("click", function() {
     alert("Hola! Soy el div");
 });
+
+let button = document.getElementById("button");
+button.addEventListener("click", function(x){
+    alert("Hola!");
+    x.stopPropagation();
+}); 
+
 });
